83f43b6b1b63983500ff14f4607dded3437152dd
bisenrahul71-spec <bisenrahul71@gmail.com>
2026-06-24 15:00:12 +0530

    Update PostJob.tsx

--- src/app/components/pages/PostJob.tsx
+++ src/app/components/pages/PostJob.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
 import { ArrowLeft } from "lucide-react"
-import API from "../../../api/config"
+import API from "../../src/api/config"
 
 export function PostJob() {
   const navigate = useNavigate()
